e6dafd565e0d9610fd92094b9aa4e31e3ffb2fb0
giasen <giammasena@gmail.com>
2022-10-16 22:37:29 +0200

    X00QD: drop hidl base and manager
    Signed-off-by: henricaodopao1 <dragonsxbr@gmail.com>

--- device.mk
+++ device.mk
@@ -213,8 +213,6 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
