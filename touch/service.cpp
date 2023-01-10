dd782c2cf1112a0aca273641c20343374d2cc10a
MLZ94 <zugutoroberto@gmail.com>
2023-01-10 22:58:59 +0100

    X00QD: touch: drop support for touchscreen gesture
    *  for now
    
    Signed-off-by: MLZ94 <zugutoroberto@gmail.com>

--- touch/service.cpp
+++ touch/service.cpp
@@ -8,17 +8,12 @@
 #include <android-base/logging.h>
 #include <binder/ProcessState.h>
 #include <hidl/HidlTransportSupport.h>
-#include "TouchscreenGesture.h"
 #include "GloveMode.h"
-
-using ::vendor::lineage::touch::V1_0::ITouchscreenGesture;
-using ::vendor::lineage::touch::V1_0::implementation::TouchscreenGesture;
 
 using ::vendor::lineage::touch::V1_0::IGloveMode;
 using ::vendor::lineage::touch::V1_0::implementation::GloveMode;
 
 int main() {
-    android::sp<ITouchscreenGesture> gestureService = new TouchscreenGesture();
     android::sp<GloveMode> gloveMode = new GloveMode();
 
     android::hardware::configureRpcThreadpool(1, true /*callerWillJoin*/);
@@ -27,17 +22,11 @@ int main() {
         LOG(ERROR) << "Can not create an instance of Touch HAL GloveMode Iface, exiting.";
         return 1;
     }
-    if (gestureService->registerAsService() != android::OK) {
-        LOG(ERROR) << "Cannot register touchscreen gesture HAL service.";
-        return 1;
-    }
     
-    LOG(INFO) << "Touchscreen HAL service ready.";
     LOG(INFO) << "GloveMode HAL service ready.";
 
     android::hardware::joinRpcThreadpool();
 
     LOG(ERROR) << "GloveMode HAL service failed to join thread pool.";
-    LOG(ERROR) << "Touchscreen HAL service failed to join thread pool.";
     return 1;
 }
