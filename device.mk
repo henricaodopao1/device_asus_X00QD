fc3db678a032425e5cf721b7771a674f46ad3e8d
henricaodopao1 <dragonsxbr@gmail.com>
2023-01-11 21:00:50 +0000

    X00QD: overlay-aosp > overlay-lineage
    Signed-off-by: henricaodopao1 <dragonsxbr@gmail.com>

--- device.mk
+++ device.mk
@@ -326,7 +326,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
